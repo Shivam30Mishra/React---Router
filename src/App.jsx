a926bde969610dbb94e493f55a5ef926065a6e34
Shivam30Mishra <theshivammishra10@gmail.com>
2025-12-17 12:41:40 +0530

    understood and written the code of outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
-import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 
 function App() {
   return (
     <div>
 
     <BrowserRouter>
-      <Link to="/">Landing</Link>
-      <Link to="/neet-online-coaching-class-11">Class11</Link>
-      <Link to="/neet-online-coaching-class-12">Class 12</Link>
-      <br />
       <Routes>
-        <Route path="/neet-online-coaching-class-11" element={<Class11Program />} />
-        <Route path="/neet-online-coaching-class-12" element={<Class12Program />} />
-        <Route path="/" element={<Landing />} />
-        <Route path="*" element={<ErrorPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/neet-online-coaching-class-11" element={<Class11Program />} />
+          <Route path="/neet-online-coaching-class-12" element={<Class12Program />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
     </div>
   )
+}
+
+function Layout() {
+  return (<div style={{height:"100vh"}}>
+    <Header />
+    <div style={{height:"90vh"}}>
+      <Outlet />
+    </div>
+  </div>)
+}
+
+function Header() {
+  return (<div>
+    <Link to={"/"}>Allen</Link>
+    <Link to={"/neet-online-coaching-class-11"}>Class 11</Link>
+    <Link to={"/neet-online-coaching-class-12"}>Class 12</Link>
+  </div>)
 }
 
 function ErrorPage() {
